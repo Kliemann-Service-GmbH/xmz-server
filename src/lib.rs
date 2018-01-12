e613a5f3535edc2efc5cedef0d7cbac51218437b
zzeroo <co@zzeroo.com>
2018-01-12 15:24:39 +0100

    Begin mit der Dokumentation

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#![doc(html_logo_url = "https://zzeroo.github.io/share/zzeroo-logo.png",
+       html_favicon_url = "https://zzeroo.github.io/share/favicon.ico",
+html_root_url = "https://gaswarnanlagen.com/")]
+//! Server Komponente der **xMZ-Plattform**.
+//!
+//! Der Server ist die Kern Komponente. Zu seinen Aufgaben zählen zum Beispiel das Auslesen der
+//! Sensoren sowie deren Auswertung und das Schalten der diversen Ausgänge, wie Relais und IO Module.
+//!
